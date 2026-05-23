4054a451055ba8e234f73b8988e412e7c746e313
Simon Kelly <26287997+simonkellly@users.noreply.github.com>
2026-05-23 14:34:57 +0100

    Add testing

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import path from "path"
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@/convex": path.resolve(__dirname, "./convex"),
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    environment: "edge-runtime",
+    include: ["convex/**/*.test.{ts,tsx,js,jsx}"],
+  },
+})
